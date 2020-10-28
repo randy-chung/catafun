0bcf55ed481eaa24904e7d95b45644deac1d178a
Randy Chung <randy.chung@gmail.com>
2020-10-28 16:36:23 -0400

    Fixed some bugs in the basic button story.

--- apps/spoilerz/src/stories/Button.stories.tsx
+++ apps/spoilerz/src/stories/Button.stories.tsx
@@ -5,20 +5,20 @@ import SplButton from './Button';
 export default {
     component: SplButton,
     title: 'Examples / Button',
-    // argTypes: {
-    //     // backgroundColor: { control: 'color' },
-    //     label: {
-    //         name: 'label',
-    //         type: { name: 'string', required: false },
-    //         defaultValue: 'Button',
-    //         description: 'This is the text that will appear on the button.'
-    //     },
-    // }
+    argTypes: {
+        // backgroundColor: { control: 'color' },
+        label: {
+            name: 'label',
+            // type: { name: 'string', required: false },
+            type: 'string',
+            defaultValue: 'Button',
+            description: 'This is the text that will appear on the button.'
+        },
+    }
 } as Meta;
 
 export const Basic = (props) => {
-    props.label = 'Basic';
     return (
-        <SplButton {...props}>{props.label}</SplButton>
+        <SplButton {...props}></SplButton>
     )
 }
